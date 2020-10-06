af908840bd9c959050a7221d544ec6d3f0eacc8b
abhishek bhaware <64492784+cosmoimai@users.noreply.github.com>
2020-10-06 20:29:48 +0530

    optimised the if-else part

--- bin/create-files.js
+++ bin/create-files.js
@@ -4,110 +4,68 @@ const fs = require("fs");
 const process = require("process");
 
 if (process.argv[2] === undefined) {
-  const readAndWriteFiles = (projectDir) => {
-    //console.log(projectDir);
-    fs.readFile(__dirname + "/index.html", (err, data) => {
-      if (err) {
-        throw err;
-      }
-      fs.writeFile(projectDir + "/index.html", data, (err) => {
-        if (err) {
-          throw err;
-        }
-        console.log("Created index.html");
-      });
-    });
-
-    // create an empty file
-    fs.open(projectDir + "/script.js", "w", (err, file) => {
-      if (err) {
-        throw err;
-      }
-
-      console.log("Created script.js");
-    });
-
-    fs.open(projectDir + "/style.css", "w", (err, file) => {
-      if (err) {
-        throw err;
-      }
-
-      console.log("Created style.css");
-    });
-  };
-
-  const dirToCreate = process.argv[2];
-  let projectDir = process.cwd();
-  if (!dirToCreate) {
-    readAndWriteFiles(projectDir);
+  var h, c, j;
+  if (
+    process.argv[3] !== undefined &&
+    process.argv[4] !== undefined &&
+    process.argv[5] !== undefined
+  ) {
+    h = process.argv[3];
+    c = process.argv[4];
+    j = process.argv[5];
   } else {
-    projectDir = projectDir + "/" + dirToCreate;
-    fs.mkdir(projectDir, (err) => {
-      if (err) {
-        throw err;
-      }
-      readAndWriteFiles(projectDir);
-    });
+    h = "index";
+    c = "style";
+    j = "script";
   }
 } else {
-  const readAndWriteFiles = (projectDir) => {
-    //console.log(projectDir);
-    var h, c, j;
-    if (
-      process.argv[3] !== undefined ||
-      process.argv[4] !== undefined ||
-      process.argv[5] !== undefined
-    ) {
-      h = "" + process.argv[3] + "";
-      c = "" + process.argv[4] + "";
-      j = "" + process.argv[5] + "";
-    } else {
-      h = "index";
-      c = "style";
-      j = "script";
+  h = "index";
+  c = "style";
+  j = "script";
+}
+const readAndWriteFiles = (projectDir) => {
+  //console.log(projectDir);
+
+  fs.readFile(__dirname + "/" + h + ".html", (err, data) => {
+    if (err) {
+      throw err;
+    }
+    fs.writeFile(projectDir + "/" + h + ".html", data, (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log("Created " + h + ".html");
+    });
+  });
+
+  // create an empty file
+  fs.open(projectDir + "/" + j + ".js", "w", (err, file) => {
+    if (err) {
+      throw err;
     }
 
-    fs.readFile(__dirname + "/" + h + ".html", (err, data) => {
-      if (err) {
-        throw err;
-      }
-      fs.writeFile(projectDir + "/" + h + ".html", data, (err) => {
-        if (err) {
-          throw err;
-        }
-        console.log("Created index.html");
-      });
-    });
+    console.log("Created " + j + ".js");
+  });
 
-    // create an empty file
-    fs.open(projectDir + "/" + j + ".js", "w", (err, file) => {
-      if (err) {
-        throw err;
-      }
+  fs.open(projectDir + "/" + c + ".css", "w", (err, file) => {
+    if (err) {
+      throw err;
+    }
 
-      console.log("Created script.js");
-    });
+    console.log("Created " + c + ".css");
+  });
+};
 
-    fs.open(projectDir + "/" + c + ".css", "w", (err, file) => {
-      if (err) {
-        throw err;
-      }
-
-      console.log("Created style.css");
-    });
-  };
-
-  const dirToCreate = process.argv[2];
-  let projectDir = process.cwd();
-  if (!dirToCreate) {
+const dirToCreate = process.argv[2];
+let projectDir = process.cwd();
+if (!dirToCreate) {
+  readAndWriteFiles(projectDir);
+} else {
+  projectDir = projectDir + "/" + dirToCreate;
+  fs.mkdir(projectDir, (err) => {
+    if (err) {
+      throw err;
+    }
     readAndWriteFiles(projectDir);
-  } else {
-    projectDir = projectDir + "/" + dirToCreate;
-    fs.mkdir(projectDir, (err) => {
-      if (err) {
-        throw err;
-      }
-      readAndWriteFiles(projectDir);
-    });
-  }
+  });
 }
